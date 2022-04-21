41fb97a700245a6c058c3c9a31ae2ffb550da938
Yegor Gursky <yegorgursky@Yegors-MacBook-Air.local>
2022-04-21 22:21:53 +0300

    refactoring forgot password

--- src/components/authorization/forgot-password/ForgotPassword.tsx
+++ src/components/authorization/forgot-password/ForgotPassword.tsx
@@ -1,3 +1,23 @@
 import React from 'react';
 
-export const ForgotPassword = () => <div />;
+import s from 'components/authorization/forgot-password/ForgotPassword.module.css';
+
+export const ForgotPassword = () => (
+  <div className={s.box}>
+    <span className={s.textCenter}>Create New Password</span>
+    <div className={s.inputContainer}>
+      <label htmlFor="inputPassword" className={s.label}>
+        <input
+          type="password"
+          id="inputPassword"
+          placeholder="Password"
+          required
+          autoComplete="off"
+        />
+      </label>
+    </div>
+    <button type="button" className={s.btn}>
+      Confirm
+    </button>
+  </div>
+);
