39810869bae84103d7d05c040f05c598132755bd
gurskygursky <yahorhurski@gmail.com>
2022-04-23 18:33:40 +0300

    add store and reducers

---
+++ src/store/reducers/index.ts
@@ -0,0 +1,5 @@
+export { appReducer } from './app-reducer';
+export { profileReducer } from './profile-reducer';
+export { signInReducer } from './signIn-reducer';
+export { signUpReducer } from './signUp-reducer';
+export { cardsReducer } from './cards-reducer';
---
+++ src/store/store.ts
@@ -0,0 +1,24 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import {
+  appReducer,
+  cardsReducer,
+  profileReducer,
+  signInReducer,
+  signUpReducer,
+} from 'store/reducers';
+
+const rootReducer = combineReducers({
+  app: appReducer,
+  signIn: signInReducer,
+  signUp: signUpReducer,
+  profilePage: profileReducer,
+  cards: cardsReducer,
+});
+// applyMiddleware supercharges createStore with middleware:
+export const store = createStore(rootReducer, applyMiddleware(thunk));
+
+// а это, чтобы можно было в консоли браузера обращаться к store в любой момент
+// @ts-ignore
+window.store = store;
