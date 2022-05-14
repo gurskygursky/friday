bca6f4af47c70bb30fc2d515607db6d11632c621
gurskygursky <yahorhurski@gmail.com>
2022-05-14 22:49:23 +0300

    add login request

--- src/api/auth-api.ts
+++ src/api/auth-api.ts
@@ -2,13 +2,39 @@ import { AxiosResponse } from 'axios';
 
 import { instance } from './api-config';
 
+export const authAPI = {
+  registration(params: RegisterParamsType) {
+    return instance.post<RegisterParamsType, AxiosResponse>(`auth/register`, params);
+  },
+  login(params: LoginParamsType) {
+    return instance.post<LoginParamsType, AxiosResponse<LoginResponseType>>(
+      `auth/login`,
+      params,
+    );
+  },
+};
+
 export type RegisterParamsType = {
   email: string;
   password: string;
 };
+export type LoginParamsType = {
+  email: string;
+  password: string;
+  rememberMe: boolean;
+};
+export type LoginResponseType = {
+  _id: string;
+  email: string;
+  name: string;
+  avatar?: string;
+  publicCardPacksCount: number; // количество колод
 
-export const authAPI = {
-  registration(params: RegisterParamsType) {
-    return instance.post<AxiosResponse>(`auth/register`, params);
-  },
+  created: Date;
+  updated: Date;
+  isAdmin: boolean;
+  verified: boolean; // подтвердил ли почту
+  rememberMe: boolean;
+
+  error?: string;
 };
