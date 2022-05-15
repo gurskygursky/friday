d1a061e386ed60a05f2e79d37027acde3d50d209
gurskygursky <yahorhurski@gmail.com>
2022-05-15 20:01:56 +0300

    edit signIn components and actions

--- src/components/authorization/sign-in/SignIn.tsx
+++ src/components/authorization/sign-in/SignIn.tsx
@@ -27,18 +27,21 @@ export const SignIn = () => {
   } = InputHook('');
   const dispatch = useDispatch();
 
-  const data = { email, password, rememberMe };
-
   const onChangeCheckbox = (e: ChangeEvent<HTMLInputElement>): void =>
     handleRememberMe(e.currentTarget.checked);
 
-  const onSubmit = (): void => {
+  const onSubmit = () => {
     if (isEmailValid(email) && isPasswordValid(password)) {
-      dispatch(loginTC({ data }));
+      // dispatch(SignInTC({ email, password, rememberMe }));
       resetEmail();
       resetPassword();
       dispatch(setAppStatusAC(requestStatus.succeeded));
     }
+  };
+  const onClickCancel = () => {
+    resetEmail();
+    resetPassword();
+    dispatch(setAppStatusAC(requestStatus.idle));
   };
 
   return (
@@ -51,22 +54,6 @@ export const SignIn = () => {
         type="email"
         name="email"
       />
-      {/* <div className={s.inputContainer}> */}
-      {/*  <label htmlFor="inputEmail" className={s.label}> */}
-      {/*    <input size={40} type="email" id="inputEmail" placeholder="Email" required /> */}
-      {/*  </label> */}
-      {/* </div> */}
-      {/* <div className={s.inputContainer}> */}
-      {/*  <label htmlFor="inputPassword" className={s.label}> */}
-      {/*    <input */}
-      {/*      size={40} */}
-      {/*      type="password" */}
-      {/*      id="inputPassword" */}
-      {/*      placeholder="Password" */}
-      {/*      required */}
-      {/*    /> */}
-      {/*  </label> */}
-      {/* </div> */}
       <CustomInput
         placeholder="Password"
         onChange={handlePassword}
@@ -74,29 +61,23 @@ export const SignIn = () => {
         type="password"
         name="password"
       />
-      <CustomInput
-        onChange={onChangeCheckbox}
-        checked={rememberMe}
-        type="checkbox"
-        name="checkbox"
-      />
-      {/* <div className={s.inputContainer}> */}
-      {/*  <label className={s.container} htmlFor="inputCheckbox"> */}
-      {/*    Remember Me */}
-      {/*    <input */}
-      {/*      id="inputCheckbox" */}
-      {/*      type="checkbox" */}
-      {/*      required={false} */}
-      {/*      name="checkbox" */}
-      {/*      value={rememberMe} */}
-      {/*      onChange={onChangeCheckbox} */}
-      {/*    /> */}
-      {/*    <span className={s.checkmark} /> */}
-      {/*  </label> */}
-      {/* </div> */}
+      <div className={s.inputContainer}>
+        <label className={s.container} htmlFor="inputCheckbox">
+          Remember Me
+          <input
+            id="inputCheckbox"
+            type="checkbox"
+            required={false}
+            name="checkbox"
+            checked={rememberMe}
+            onChange={onChangeCheckbox}
+          />
+          <span className={s.checkmark} />
+        </label>
+      </div>
       <CustomButton title="Sign In" onClick={onSubmit} />
       <NavLink to={PATH.REGISTRATION_PAGE}>
-        <CustomButton title="Sign Up" onClick={() => {}} />
+        <CustomButton title="Sign Up" onClick={onClickCancel} />
       </NavLink>
     </div>
   );
--- src/store/reducers/signIn-reducer.ts
+++ src/store/reducers/signIn-reducer.ts
@@ -1,13 +1,73 @@
-const initialState = {};
+import { ThunkDispatch } from 'redux-thunk';
 
-// Use the initialState as a default value
-export const signInReducer = (state = initialState, action: any) => {
-  // The reducer normally looks at the action type field to decide what happens
+import { setAppStatusAC } from './app-reducer';
+
+import { authAPI, LoginParamsType } from 'api/auth-api';
+import { Nullable } from 'components/types';
+import { ACTIONS_TYPE } from 'enums/actions';
+import { requestStatus } from 'enums/request';
+import { RootStoreType } from 'store/store';
+
+export type InitialStateDataType = {
+  isAuth: boolean;
+  error?: Nullable<string>;
+};
+
+export const initialState: InitialStateDataType = {
+  isAuth: false,
+  error: null,
+};
+
+export const signInReducer = (
+  state: InitialStateDataType = initialState,
+  action: ActionTypesSignIn,
+): InitialStateDataType => {
   switch (action.type) {
-    // Do something here based on the different types of actions
+    case ACTIONS_TYPE.SET_AUTH_SIGN_IN_DATA:
+      return {
+        ...state,
+        isAuth: action.isAuth,
+      };
+    case ACTIONS_TYPE.SET_ERROR_MESSAGE:
+      return {
+        ...state,
+        error: action.error,
+      };
     default:
-      // If this reducer doesn't recognize the action type, or doesn't
-      // care about this specific action, return the existing state unchanged
       return state;
   }
 };
+
+export const setAuthSignInDataAC = (isAuth: boolean) =>
+  ({ type: ACTIONS_TYPE.SET_AUTH_SIGN_IN_DATA, isAuth } as const);
+
+export const setErrorMessageAC = (error: Nullable<string>) =>
+  ({ type: ACTIONS_TYPE.SET_ERROR_MESSAGE, error } as const);
+
+export const SignInTC =
+  (data: LoginParamsType) =>
+  (
+    dispatch: ThunkDispatch<
+      RootStoreType,
+      undefined,
+      ActionTypesSignIn | ReturnType<typeof setAppStatusAC>
+    >,
+  ) => {
+    dispatch(setAppStatusAC(requestStatus.loading));
+    authAPI
+      .login(data)
+      .then(() => {
+        dispatch(setAuthSignInDataAC(true));
+        // dispatch(setUserProfile(res.data));
+        dispatch(setAppStatusAC(requestStatus.succeeded));
+      })
+      .catch(() => {})
+      .finally(() => {
+        dispatch(setAppStatusAC(requestStatus.idle));
+      });
+  };
+
+// type;
+export type setLoginData = ReturnType<typeof setAuthSignInDataAC>;
+type setErrorMessageLogin = ReturnType<typeof setErrorMessageAC>;
+export type ActionTypesSignIn = setLoginData | setErrorMessageLogin;
