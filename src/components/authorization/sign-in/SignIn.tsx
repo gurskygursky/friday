dd69621c6d535f6a9869d651380b9e99cf98f5b8
gurskygursky <yahorhurski@gmail.com>
2022-05-15 17:59:05 +0300

    edit custom input and add functionality to signIn components

--- src/components/authorization/sign-in/SignIn.tsx
+++ src/components/authorization/sign-in/SignIn.tsx
@@ -1,40 +1,103 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 
+import { useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
 import s from 'components/authorization/sign-in/SignIn.module.css';
 import { CustomButton } from 'components/custom-button';
+import { CustomInput } from 'components/custom-input';
+import { InputHook } from 'components/hooks/input-hook/Input';
 import { PATH } from 'enums/pathes';
+import { requestStatus } from 'enums/request';
+import { isEmailValid } from 'helpers/authorization/emailValidator';
+import { isPasswordValid } from 'helpers/authorization/passwordValidator';
+import { setAppStatusAC } from 'store/reducers/app-reducer';
 
-export const SignIn = () => (
-  <div className={s.box}>
-    <span className={s.textCenter}>login</span>
-    <div className={s.inputContainer}>
-      <label htmlFor="inputEmail" className={s.label}>
-        <input size={40} type="email" id="inputEmail" placeholder="Email" required />
-      </label>
+export const SignIn = () => {
+  const [rememberMe, handleRememberMe] = useState<boolean>(false);
+  const {
+    inputValues: email,
+    handleValueOnChange: handleEmail,
+    handleResetValueOnChange: resetEmail,
+  } = InputHook('');
+  const {
+    inputValues: password,
+    handleValueOnChange: handlePassword,
+    handleResetValueOnChange: resetPassword,
+  } = InputHook('');
+  const dispatch = useDispatch();
+
+  const data = { email, password, rememberMe };
+
+  const onChangeCheckbox = (e: ChangeEvent<HTMLInputElement>): void =>
+    handleRememberMe(e.currentTarget.checked);
+
+  const onSubmit = (): void => {
+    if (isEmailValid(email) && isPasswordValid(password)) {
+      dispatch(loginTC({ data }));
+      resetEmail();
+      resetPassword();
+      dispatch(setAppStatusAC(requestStatus.succeeded));
+    }
+  };
+
+  return (
+    <div className={s.box}>
+      <span className={s.textCenter}>login</span>
+      <CustomInput
+        placeholder="Email"
+        onChange={handleEmail}
+        value={email}
+        type="email"
+        name="email"
+      />
+      {/* <div className={s.inputContainer}> */}
+      {/*  <label htmlFor="inputEmail" className={s.label}> */}
+      {/*    <input size={40} type="email" id="inputEmail" placeholder="Email" required /> */}
+      {/*  </label> */}
+      {/* </div> */}
+      {/* <div className={s.inputContainer}> */}
+      {/*  <label htmlFor="inputPassword" className={s.label}> */}
+      {/*    <input */}
+      {/*      size={40} */}
+      {/*      type="password" */}
+      {/*      id="inputPassword" */}
+      {/*      placeholder="Password" */}
+      {/*      required */}
+      {/*    /> */}
+      {/*  </label> */}
+      {/* </div> */}
+      <CustomInput
+        placeholder="Password"
+        onChange={handlePassword}
+        value={password}
+        type="password"
+        name="password"
+      />
+      <CustomInput
+        onChange={onChangeCheckbox}
+        checked={rememberMe}
+        type="checkbox"
+        name="checkbox"
+      />
+      {/* <div className={s.inputContainer}> */}
+      {/*  <label className={s.container} htmlFor="inputCheckbox"> */}
+      {/*    Remember Me */}
+      {/*    <input */}
+      {/*      id="inputCheckbox" */}
+      {/*      type="checkbox" */}
+      {/*      required={false} */}
+      {/*      name="checkbox" */}
+      {/*      value={rememberMe} */}
+      {/*      onChange={onChangeCheckbox} */}
+      {/*    /> */}
+      {/*    <span className={s.checkmark} /> */}
+      {/*  </label> */}
+      {/* </div> */}
+      <CustomButton title="Sign In" onClick={onSubmit} />
+      <NavLink to={PATH.REGISTRATION_PAGE}>
+        <CustomButton title="Sign Up" onClick={() => {}} />
+      </NavLink>
     </div>
-    <div className={s.inputContainer}>
-      <label htmlFor="inputPassword" className={s.label}>
-        <input
-          size={40}
-          type="password"
-          id="inputPassword"
-          placeholder="Password"
-          required
-        />
-      </label>
-    </div>
-    <div className={s.inputContainer}>
-      <label className={s.container} htmlFor="inputCheckbox">
-        Remember Me
-        <input id="inputCheckbox" type="checkbox" required={false} />
-        <span className={s.checkmark} />
-      </label>
-    </div>
-    <CustomButton title="Sign In" onClick={() => {}} />
-    <NavLink to={PATH.REGISTRATION_PAGE}>
-      <CustomButton title="Sign Up" onClick={() => {}} />
-    </NavLink>
-  </div>
-);
+  );
+};
--- src/components/custom-input/CustomInput.tsx
+++ src/components/custom-input/CustomInput.tsx
@@ -3,16 +3,17 @@ import React, { ChangeEvent, memo } from 'react';
 import s from 'components/authorization/sign-in/SignIn.module.css';
 
 type PropsType = {
-  placeholder: string;
+  placeholder?: string;
   type: string;
   name?: string;
   className?: string;
-  value: string;
+  value?: string;
+  checked?: boolean;
   onChange: (event: ChangeEvent<HTMLInputElement>) => void;
 };
 
 export const CustomInput = memo((props: PropsType) => {
-  const { placeholder, type, name, className, value, onChange } = props;
+  const { placeholder, type, name, className, value, onChange, checked } = props;
   return (
     <div className={s.inputContainer}>
       <label htmlFor="inputPassword" className={s.label}>
@@ -23,6 +24,7 @@ export const CustomInput = memo((props: PropsType) => {
           className={className}
           value={value}
           onChange={onChange}
+          checked={checked}
         />
       </label>
     </div>
