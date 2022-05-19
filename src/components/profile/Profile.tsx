81c8ce9d8f73926e50288f310eac96da556a3d47
gurskygursky <yahorhurski@gmail.com>
2022-05-19 21:49:08 +0300

    add logic and layout to profile component

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,3 +1,39 @@
 import React from 'react';
 
-export const Profile = () => <div />;
+import { useDispatch, useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+
+import s from 'components/authorization/sign-in/SignIn.module.css';
+import { CustomButton } from 'components/custom-button';
+import { PATH } from 'enums/pathes';
+import { AppState } from 'store/store';
+
+export const Profile = () => {
+  const dispatch = useDispatch();
+  const isAuth = useSelector<AppState, boolean>(state => state.signIn.isAuth);
+
+  const onClickLogout = () => {
+    dispatch(LogoutTC);
+  };
+
+  if (!isAuth) {
+    return <Navigate to={PATH.LOGIN_PAGE} />;
+  }
+
+  return (
+    <div className={s.box}>
+      <span className={s.textCenter}>Profile</span>
+      <input
+        type="file"
+        alt="image-input"
+        name="image-input"
+        id="image-input"
+        accept=".jpg, .jpeg, .png"
+        onChange={() => {}}
+      />
+      <div className={s.btn}>
+        <CustomButton title="LOGOUT" onClick={onClickLogout} />
+      </div>
+    </div>
+  );
+};
