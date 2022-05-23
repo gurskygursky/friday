dfe316378de47f09f2fd019a7000044d89d6c639
gurskygursky <yahorhurski@gmail.com>
2022-05-23 15:30:05 +0300

    fixed pathes

--- src/enums/pathes.ts
+++ src/enums/pathes.ts
@@ -3,6 +3,6 @@ export enum PATH {
   REGISTRATION_PAGE = '/registration',
   LOGIN_PAGE = '/login',
   PROFILE_PAGE = '/profile',
-  FORGOT_PASSWORD_PAGE = '/forgot-password/*',
+  FORGOT_PASSWORD_PAGE = '/forgot-password',
   SET_NEW_PASSWORD_PAGE = '/set-new-password/:token',
 }
